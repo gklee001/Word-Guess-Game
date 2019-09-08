0d4997d28b178dd8158cc2f104c8deed4e48ce7c
Kneeslapper <gloriaklee8@gmail.com>
2019-09-08 15:45:29 -0700

    added a reset.css page

---
+++ assets/javascript/games.js
@@ -0,0 +1,5 @@
+// was told to put code in .js//
+
+//first step is to write a code to guess what letter the computer is thinking//
+
+var computerguess = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
