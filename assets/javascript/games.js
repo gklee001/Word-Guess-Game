55c858ad86d7f78e1229a18b0c160f2d4ac2675b
Kneeslapper <gloriaklee8@gmail.com>
2019-09-09 16:54:49 -0700

    editing line 17, console logged it as syntax error, deleted { and }

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -7,17 +7,16 @@ var computerOptions = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l
 //now create variables for wins and loses and numbers of guesses for humans and computer, the variables start at 0, then increase//
 
 var humanGuess = 0;
-var computerGuess = 0;
+// var computerGuess = 0;
 var wins = 0;
 var lose = 0;
 var humanGuessAllowed = 10;
 
 //now we need to create a function to capture the users guess//
 //this was something I used from the rock paper scissor activity//
-document.onkeypress = fuction(event) {
-    var humanGuess = event.key;
-    document.getElementById("demo").innerHTML = "The pressed key was: " + humanGuess;
-}
+document.onkeypress = fuction(event)
+var humanGuess = event.key;
+document.getElementById("humanGuess").innerHTML = "The pressed key was: " + humanGuess;
 
 console.log(humanGuess);
 
@@ -26,17 +25,14 @@ var computerGuess = computerOptions[Math.floor(Math.random() * computerOptions.l
 
 //need to create a condition that allows human guess up to 10 times, maybe do a loop?
 
-//now I should use else if to give conditions for win or lose//
+//now I should use else if to give conditions for win or lose also need to decrease the number of guesses also
 if (humanGuess === computerOption) {
-    wins++
+    wins++;
 }
 
 else (humanGuess !== computerGuess) {
-    lose++
+    lose++;
+    humanGuessAllowed--;
 }
-//need to decrease the number of guesses also
 
-if (humanGuessAllowed = 0) {
-    lose++
-}
 
