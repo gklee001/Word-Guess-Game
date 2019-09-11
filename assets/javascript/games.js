15380f8e16d70bd4d14547002fb4d75d20c39194
Kneeslapper <gloriaklee8@gmail.com>
2019-09-11 16:50:38 -0700

    the wins are now appearing on screen, due to changing if statuement from humanGuessAllowed-- to humanGuessAllowed= 10, but its showing a win everytime

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -1,19 +1,36 @@
-// was told to put code in .js//
-
-//first step is to write a code to guess what letter the computer is thinking//
+//first step is to write a code to guess what letter the computer could be thinking//
 
 var computerOptions = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
 
-//now create variables for wins and loses and numbers of guesses for humans and computer, the variables start at 0, then increase//
-
-var humanGuess = 0;
-var computerGuess = 0;
-var wins = 0;
+var win = 0;
 var lose = 0;
 var humanGuessAllowed = 10;
-
+var humanGuess = 0;
 //need to declare an empty array for left over guesses
-var arrayName = [];
+
+
+//now we create function to have the computer randomly pick a letter//
+
+var computerGuess = computerOptions[Math.floor(Math.random() * computerOptions.length)];
+console.log(computerGuess)
+
+//need to create a condition that allows human guess up to 10 times, maybe do a loop? now I should use else if to give conditions for win or lose also need to decrease the number of guesses also
+if (humanGuess == computerGuess) {
+    win++;
+    humanGuessAllowed = 10;
+    humanGuess = 0;
+
+}
+if (humanGuess !== computerGuess) {
+    humanGuessAllowed--;
+}
+
+if (humanGuessAllowed == 0) {
+    lose++;
+    humanGuessAllowed = 10;
+
+}
+
 
 //document.getElementById("#humanGuess").innerHTML = humanGuess;
 //document.getElementById("#computerGuess").innerHTML = computerGuess;
@@ -21,31 +38,22 @@ var arrayName = [];
 
 //now we need to create a function to capture the users guess, this was something I used from the rock paper scissor activity//  
 document.onkeyup = function (event) {
+
     humanGuess = event.key;
     document.getElementById("humanGuess").innerHTML = "The pressed key was: " + humanGuess;
-    arrayName.push(humanGuess);
+    console.log(humanGuess)
+
+    document.getElementById("humanGuessAllowed").innerHTML =
+        humanGuessAllowed;
+
+    document.getElementById("win").innerHTML =
+        win++;
+    document.getElementById("lose").innerHTML =
+        lose;
+
 }
+console.log(document.onkeyup)
 
-
-//now we create function to have the computer randomly pick a letter//
-
-var computerGuess = computerOptions[Math.floor(Math.random() * computerOptions.length)];
-
-//need to create a condition that allows human guess up to 10 times, maybe do a loop?
-
-
-//now I should use else if to give conditions for win or lose also need to decrease the number of guesses also
-if (humanGuess == computerGuess) {
-    wins++;
-    humanGuessAllowed = 10;
-    arrayName = [];
-}
-if (humanGuess !== computerGuess) {
-    humanGuessAllowed--;
-}
-else if (humanGuessAllowed == 0) {
-    lose++;
-}
 
 
 
