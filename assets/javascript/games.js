df019d4568fb2d7fb646f57594e7ab4ca287ac82
Kneeslapper <gloriaklee8@gmail.com>
2019-09-11 18:29:10 -0700

    moved the condition into the function

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -6,30 +6,7 @@ var win = 0;
 var lose = 0;
 var humanGuessAllowed = 10;
 var humanGuess = 0;
-//need to declare an empty array for left over guesses
 
-
-//now we create function to have the computer randomly pick a letter//
-
-var computerGuess = computerOptions[Math.floor(Math.random() * computerOptions.length)];
-console.log(computerGuess)
-
-//need to create a condition that allows human guess up to 10 times, maybe do a loop? now I should use else if to give conditions for win or lose also need to decrease the number of guesses also
-if (humanGuess == computerGuess) {
-    win++;
-    humanGuessAllowed = 10;
-    humanGuess = 0;
-
-}
-if (humanGuess !== computerGuess) {
-    humanGuessAllowed--;
-}
-
-else if (humanGuessAllowed === 0) {
-    lose++;
-    humanGuessAllowed = 10;
-    console.log(humanGuessAllowed)
-}
 
 
 //document.getElementById("#humanGuess").innerHTML = humanGuess;
@@ -43,20 +20,47 @@ document.onkeyup = function (event) {
     document.getElementById("humanGuess").innerHTML = "The pressed key was: " + humanGuess;
     console.log(humanGuess)
 
+
+    if (humanGuess == computerGuess) {
+        win++;
+        humanGuessAllowed = 10;
+        humanGuess = 0;
+
+    }
+    if (humanGuess !== computerGuess) {
+        humanGuessAllowed--;
+    }
+
+    else if (humanGuessAllowed === 0) {
+        lose++;
+        humanGuessAllowed = 10;
+        console.log(humanGuessAllowed)
+    }
+
     document.getElementById("humanGuessAllowed").innerHTML =
         humanGuessAllowed;
 
     document.getElementById("win").innerHTML =
-        win++;
+        win;
     document.getElementById("lose").innerHTML =
         lose;
 
-}
 
-//want to display this information onto HTML
-humanGuessAllowed.textContent = humanGuessAllowed;
-win.textContent = win;
-console.log(document.onkeyup)
+    //add another reset function
+
+};
+
+//want to display this information onto HTML??? Its not working,
+
+//need to declare an empty array for left over guesses
+
+
+//now we create function to have the computer randomly pick a letter//
+
+var computerGuess = computerOptions[Math.floor(Math.random() * computerOptions.length)];
+console.log(computerGuess)
+
+//need to create a condition that allows human guess up to 10 times, maybe do a loop? now I should use else if to give conditions for win or lose also need to decrease the number of guesses also
 
 
 
